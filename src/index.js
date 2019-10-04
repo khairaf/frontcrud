380c5f804e13fb86148c7caad32ab2b8be68f025
khairaf <khairanifitratul@gmail.com>
2019-10-05 06:03:02 +0700

    hmm

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
+//tes dulu haiii
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
 
